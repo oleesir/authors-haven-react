feb49c4db0e602b537b7a5f14d72a4f5d3e33d13
oleesir <oleesir@gmail.com>
2020-04-13 10:12:46 +0100

    ch(setup-circleci):Implement  ch-setup-circleci
    - Implement ch-setup-circleci
    
    [Finishes #168675524]

--- src/__tests__/App.spec.jsx
+++ src/__tests__/App.spec.jsx
@@ -1,5 +1,10 @@
-describe('My Test Suite', () => {
-  it('My Test Case', () => {
-    expect(true).toEqual(true);
-  });
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from '../components/App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/You just setup react with babel and eslint/i);
+  expect(linkElement).toBeInTheDocument();
 });
